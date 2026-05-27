f9dc55ff5eb267ebc20f555fb684d82a5a27c879
safwanahendouz <safwanahendouz241@gmail.com>
2026-05-27 21:57:53 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -8,8 +8,24 @@ class RegistrationForm(App):
         self.title = "Registration Form"
         layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
         
-        head_label = Label(text="Registration Form", font_size=26, bold=True)
+        head_label = Label(text="Registration Form", font_size=26, bold=True ,height=40)
+        # Adding label
+        name_label = Label(text="Name:", font_size=18, height=30)
+        self.name_input = TextInput(multiline=False, font_size=18)
+
+        name_label = Label(text="Name:", font_size=18, height=30)
+        self.name_input = TextInput(multiline=False, font_size=18)
+
+        name_label = Label(text="Name:", font_size=18, height=30)
+        self.name_input = TextInput(multiline=False, font_size=18)
+        
+        name_label = Label(text="Name:", font_size=18, height=30)
+        self.name_input = TextInput(multiline=False, font_size=18)
+
+        # Adding widgets to layout
         layout.add_widget(head_label)
+        layout.add_widget(name_label)
+        layout.add_widget(self.name_input)
 
         return layout
 
